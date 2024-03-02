b938b6c1b8effe54c3f6093cae26aa199c2bac2e
jonas.martinez <cbobbxta.fr@gmail.com>
2024-03-02 11:57:12 +0100

    Secret testing

--- src/listeners/increment.js
+++ src/listeners/increment.js
@@ -10,6 +10,9 @@ import { Counter } from "../classes/Counter.js";
  * @returns 
  */
 export default async function (props, _event, api) {
+    console.log("I found your secret !!")
+    console.log(process.env.REALLY_SECRET);
+
     await api.data.coll(Counter).updateMany({ _id: props.id }, {
         $inc: {
             count: 1
